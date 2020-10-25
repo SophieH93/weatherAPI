d4ad6ccf8f0b9a77b5500e5558036ee5fb21c76c
sophie <sophiehickey123@gmail.com>
2020-10-25 17:51:32 +0000

    added data to display weahter in html

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
  */
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
-  //HINT: Use template literals to create a url with input and an API key
-
-  //CODE GOES HERE
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
+  const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
 }
+
 
 /**
  * Retrieve city input and get the weather data
@@ -27,8 +30,13 @@ getWeatherData = (city) => {
  */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
-  // CODE GOES HERE
-
+  getWeatherData(city)
+    .then((response) => {
+      console.log(response);
+      showWeatherData(response);
+    }).catch((error) => {
+      console.log(error);
+    })
 }
 
 /**
@@ -36,7 +44,10 @@ searchCity = () => {
  * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
-  
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
 }
 
